5b6da5bbd805b8ebe117fb8af0b01a215a01837f
hwngnt <hwng.nt@outlook.com>
2021-09-24 14:17:35 +0700

    init admin

---
+++ admin.js
@@ -0,0 +1,20 @@
+const express = require('express');
+var router = express.Router();
+
+//localhost/admin
+
+router.get('/admin', (req, res)=>{
+    res.render('admin')
+})
+
+
+//localhost/admin/addUser
+router.get('/adminAddStaff', (req, res)=>{
+    res.render('addUser')
+})
+
+router.get('/adminAddTrainer', (req, res)=>{
+    res.render('addUser')
+})
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express()
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.use(express.static(__dirname + '/public'));
+var adminController = require('./admin.js');
+app.use('/', adminController);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+console.log('listening on port' + PORT);
