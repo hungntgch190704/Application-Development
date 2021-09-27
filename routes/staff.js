a215b5fe03c80aaa5400b14f14cb9fea1ecc07cb
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2021-09-28 01:38:01 +0700

    ok

--- routes/staff.js
+++ routes/staff.js
@@ -2,23 +2,40 @@ const express = require('express');
 const router = express.Router();
 
 //localhost/admin
+router.post('/login', (req, res)=>{
+    res.redirect("/staff")
+});
 
 router.get('/staff', (req, res)=>{
     res.render('staffIndex')
-})
-
-router.post('/login', (req, res)=>{
-    res.redirect("/staff")
-})
+});
+//trainee
 router.get('/staff/trainee', (req, res) => {
     res.render('staffTrainee')
 });
+
+router.get('/staff/trainee/add', (req, res) => {
+    res.render('staffAddTrainee')
+});
+
+router.get('/staff/trainee/edit', (req, res) => {
+    res.render('staffEditTrainee')
+});
+
+//Course Category
 router.get('/staff/courseCategory', (req, res) => {
     res.render('staffCourseCategory')
 });
+
 router.get('/staff/courseCategory/add', (req, res) => {
     res.render('staffAddCourseCategory')
 });
+
+router.get('/staff/courseCategory/edit', (req, res) => {
+    res.render('staffEditCourseCategory')
+});
+
+// Course
 router.get('/staff/course', (req, res) => {
     res.render('staffCourse')
 });
