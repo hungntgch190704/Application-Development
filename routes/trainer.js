01b8fbc1a8b39a833d62a9330d489246197d0a88
Do Duc Tai <taiddgch190834@fpt.edu.vn>
2021-10-07 07:46:02 +0700

    update

--- routes/trainer.js
+++ routes/trainer.js
@@ -9,9 +9,9 @@ router.get('/trainer/trainerEdit', isTrainer, trainerController.editTrainer);
 
 router.post('/trainer/trainerEdit/doEditTrainer', isTrainer, trainerController.updateTrainer);
 
-router.get('/trainer/AssignedCourses', isTrainer, trainerController.viewAllCategory);
+router.get('/trainer/AssignedCourses', isTrainer, trainerController.viewCourse);
 
-router.post('trainer/searchCourse', isTrainer, trainerController.searchCourse);
+router.post('trainer/trainerSearchCourse', isTrainer, trainerController.searchCourse);
 
 router.get('/trainer/viewTrainee', isTrainer, trainerController.viewTrainee);
 
