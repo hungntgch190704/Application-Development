f5201da0e9139e95b6c1774bf46fdfc53b5890a1
Nguyen Thi Lan Anh <81799232+lananh20111999@users.noreply.github.com>
2021-10-25 14:11:48 +0700

    update

--- controller/admin.js
+++ controller/admin.js
@@ -5,17 +5,17 @@ const validation = require('./validation');
 const courseDetail = require('../models/courseDetail');
 const bcrypt = require('bcrypt');
 
-exports.getAdmin = async (req, res) =>{
-    res.render('admin', {loginName : req.session.email})
+exports.getAdmin = async (req, res) => {
+    res.render('admin', { loginName: req.session.email })
 }
 
 // add new staff
-exports.addStaff =  (req, res) =>{
+exports.addStaff = (req, res) => {
     let newStaff = new staff({
         name: req.body.name,
-        email:req.body.email,
+        email: req.body.email,
         age: req.body.age,
-        address:req.body.address,
+        address: req.body.address,
         img: req.file.filename
     })
     let newAccount = new Account({
@@ -24,24 +24,24 @@ exports.addStaff =  (req, res) =>{
         Role: "staff"
     })
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newAccount.password, salt, (err,hash) => {
-            if(err) throw err;
+        bcrypt.hash(newAccount.password, salt, (err, hash) => {
+            if (err) throw err;
             newAccount.password = hash;
-            newStaff =  newStaff.save();
-            newAccount =  newAccount.save();
+            newStaff = newStaff.save();
+            newAccount = newAccount.save();
         })
     })
     res.redirect('/admin/adminViewStaff');
 }
 
 //add new trainer
-exports.addTrainer =  (req, res) =>{
+exports.addTrainer = (req, res) => {
     let newTrainer = new trainer({
         name: req.body.name,
-        email:req.body.email,
+        email: req.body.email,
         speciality: req.body.speciality,
         age: req.body.age,
-        address:req.body.address,
+        address: req.body.address,
         img: req.file.filename
     })
     let newAccount = new Account({
@@ -50,75 +50,75 @@ exports.addTrainer =  (req, res) =>{
         Role: "trainer"
     })
     bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newAccount.password, salt, (err,hash) => {
-            if(err) throw err;
+        bcrypt.hash(newAccount.password, salt, (err, hash) => {
+            if (err) throw err;
             newAccount.password = hash;
-            newTrainer =  newTrainer.save();
-            newAccount =  newAccount.save();
+            newTrainer = newTrainer.save();
+            newAccount = newAccount.save();
         })
     })
     res.redirect('/admin/adminViewTrainer');
 }
 
 exports.getAddTrainer = async (req, res) => {
-    res.render('adminAddTrainer', {loginName : req.session.email});
+    res.render('adminAddTrainer', { loginName: req.session.email });
 }
 
 exports.getAddStaff = async (req, res) => {
-    res.render('adminAddStaff', {loginName : req.session.email});
+    res.render('adminAddStaff', { loginName: req.session.email });
 }
 
 //view all trainer
-exports.viewTrainer = async (req, res) =>{
+exports.viewTrainer = async (req, res) => {
     let listTrainer = await trainer.find();
-    res.render('adminViewTrainer', {listTrainer: listTrainer, loginName : req.session.email} )
+    res.render('adminViewTrainer', { listTrainer: listTrainer, loginName: req.session.email })
 }
 
 //view all staff
-exports.viewStaff = async (req, res) =>{
+exports.viewStaff = async (req, res) => {
     let listStaff = await staff.find();
-    res.render('adminViewStaff', {listStaff: listStaff, loginName : req.session.email})
+    res.render('adminViewStaff', { listStaff: listStaff, loginName: req.session.email })
 }
 
-exports.editStaff = async (req, res) =>{
+exports.editStaff = async (req, res) => {
     let id = req.query.id;
     let aStaff = await staff.findById(id);
     // console.log(aStaff);
-    res.render('adminEditStaff',{aStaff: aStaff, loginName : req.session.email})
+    res.render('adminEditStaff', { aStaff: aStaff, loginName: req.session.email })
 }
 
-exports.updateStaff = async (req, res) =>{
+exports.updateStaff = async (req, res) => {
     let id = req.body.id;
     let aStaff = await staff.findById(id);
-    if(req.file){
+    if (req.file) {
         aStaff.img = req.file.filename;
     }
     aStaff.name = req.body.name;
     aStaff.email = req.body.email;
     aStaff.age = req.body.age;
     aStaff.address = req.body.address;
-    
-    try{
+
+    try {
         aStaff = await aStaff.save();
         res.redirect('/admin/adminViewStaff');
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.redirect('/admin/adminViewStaff');
     }
 }
 
-exports.editTrainer = async (req, res) =>{
+exports.editTrainer = async (req, res) => {
     let id = req.query.id;
     let aTrainer = await trainer.findById(id);
-    res.render('adminEditTrainer',{aTrainer: aTrainer, loginName : req.session.email})
+    res.render('adminEditTrainer', { aTrainer: aTrainer, loginName: req.session.email })
 }
 
-exports.updateTrainer = async (req, res) =>{
+exports.updateTrainer = async (req, res) => {
     let id = req.body.id;
     let aTrainer = await trainer.findById(id);
     console.log(aTrainer)
-    if(req.file){
+    if (req.file) {
         aTrainer.img = req.file.filename;
         console.log(req.file.filename);
     }
@@ -128,11 +128,11 @@ exports.updateTrainer = async (req, res) =>{
     aTrainer.speciality = req.body.speciality;
     aTrainer.age = req.body.age;
     aTrainer.address = req.body.address;
-    try{
+    try {
         aTrainer = await aTrainer.save();
         res.redirect('/admin/adminViewTrainer');
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.redirect('/admin/adminViewTrainer');
     }
@@ -146,11 +146,11 @@ exports.deleteStaff = async (req, res) => {
     Account.deleteOne({ 'email': email }, (err) => {
         if (err)
             throw err;
-        else 
+        else
             console.log('Account is deleted');
     })
-    staff.findByIdAndRemove(id).then(data={});
-    
+    staff.findByIdAndRemove(id).then(data = {});
+
     res.redirect('/admin/adminViewStaff');
 }
 
@@ -162,126 +162,119 @@ exports.deleteTrainer = async (req, res) => {
     Account.deleteOne({ 'email': email }, (err) => {
         if (err)
             throw err;
-        else 
+        else
             console.log('Account is deleted');
     })
-    trainer.findByIdAndRemove(id).then(data={});
+    trainer.findByIdAndRemove(id).then(data = {});
     console.log(aTrainer.name);
-    await courseDetail.deleteMany({trainer: aTrainer.name});
+    await courseDetail.deleteMany({ trainer: aTrainer.name });
     res.redirect('/admin/adminViewTrainer');
 }
 
-exports.searchStaff= async (req, res) => {
+exports.searchStaff = async (req, res) => {
     const searchText = req.body.keyword;
     //console.log(req.body.keyword);
     let listStaff;
     let checkAlphaName = validation.checkAlphabet(searchText);
     let checkEmpty = validation.checkEmpty(searchText);
-    const searchCondition = new RegExp(searchText,'i');
+    const searchCondition = new RegExp(searchText, 'i');
 
     //console.log(checkEmpty);
-    if(!checkEmpty){
+    if (!checkEmpty) {
         res.redirect('/admin/adminViewStaff');
     }
-    else if(checkAlphaName){
-        listStaff = await staff.find({name: searchCondition});
+    else if (checkAlphaName) {
+        listStaff = await staff.find({ name: searchCondition });
     }
-    res.render('adminViewStaff', { listStaff: listStaff , loginName : req.session.email});
+    res.render('adminViewStaff', { listStaff: listStaff, loginName: req.session.email });
 }
 
-exports.searchTrainer= async (req, res) => {
+exports.searchTrainer = async (req, res) => {
     const searchText = req.body.keyword;
     //console.log(req.body.keyword);
     let listTrainer;
     let checkAlphaName = validation.checkAlphabet(searchText);
     let checkEmpty = validation.checkEmpty(searchText);
-    const searchCondition = new RegExp(searchText,'i');
+    const searchCondition = new RegExp(searchText, 'i');
 
     //console.log(checkEmpty);
-    if(!checkEmpty){
+    if (!checkEmpty) {
         res.redirect('/admin/adminViewTrainer');
     }
-    else if(checkAlphaName){
-        listTrainer = await trainer.find({name: searchCondition});
+    else if (checkAlphaName) {
+        listTrainer = await trainer.find({ name: searchCondition });
     }
-    res.render('adminViewTrainer', { listTrainer: listTrainer, loginName : req.session.email });
+    res.render('adminViewTrainer', { listTrainer: listTrainer, loginName: req.session.email });
 }
 
-exports.setDefaultPass =  (req, res)=> {
-    let id = req.query.id; 
+exports.setDefaultPass = (req, res) => {
+    let id = req.query.id;
     console.log(id);
-    let aStaff =  staff.findById(id);
-    let account =  Account.findOne({ 'email': aStaff.email }).exec()
+    let aStaff = staff.findById(id);
+    let account = Account.findOne({ 'email': aStaff.email }).exec()
     account.password = "12345678";
-    try{
+    try {
         bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(account.password, salt, (err, hash) => {
-                if(err) throw err;
+                if (err) throw err;
                 account.password = hash;
-                account =  account.save();
+                account = account.save();
             })
         })
         // account = await account.save();
         res.redirect('/admin/adminViewStaff');
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.redirect('/admin/adminViewStaff');
     }
 }
 
 exports.getChangePass = (req, res) => {
-    res.render('/changePass', { loginName: req.session.email })
+    res.render('changePass', { loginName: req.session.email })
 }
 
-exports.doChangePass = (req, res) => {
-    let acc = Account.findOne({ email: req.session.email });
+exports.doChangePass = async (req, res) => {
+    let user = await Account.findOne({ email: req.session.email });
     let current = req.body.current;
     let newpw = req.body.new;
     let confirm = req.body.confirm;
-    let errors=[];
+    let errors = {};
     let flag = true;
-    try{
-        Account.findOne({email:req.session.email})
-        .then(user => {
-            bcrypt.compare(current, user.password)
+    try {
+        await bcrypt.compare(current, user.password)
             .then((doMatch) => {
-                if(doMatch) {
-                    if(newpw.length <8){
-                        flag=false;
-                        errors[length] = "Password must contain 8 characters or more!";
+                if (doMatch) {
+                    if (newpw.length < 8) {
+                        flag = false;
+                        Object.assign(errors, { length: "Password must contain 8 characters or more!" });
                     }
-                    if(newpw != confirm){
-                        flag=false;
-                        errors[check] = "New Password and Confirm Password do not match!"
+                    else if (newpw != confirm) {
+                        flag = false;
+                        Object.assign(errors, { check: "New Password and Confirm Password do not match!" });
                     }
                 }
-                else{
+                else {
                     flag = false;
-                    errors[current] = "Old password is incorrect!";
+                    Object.assign(errors, { current: "Old password is incorrect!" });
                 }
-            })
-        })
-    } catch(err){
-        console.log(err);
-    }
-    if(!flag){
-        res.render('changePass', {errors: errors, loginName: req.session.email})
-    }
-    else{
-        try{
-            bcrypt.genSalt(10, (err, salt) => {
+            });
+        if (!flag) {
+            res.render('changePass', { errors: errors, loginName: req.session.email })
+        }
+        else {
+            await bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(newpw, salt, (err, hash) => {
-                    if(err) throw err;
-                    acc.password = hash;
-                    acc =  acc.save();
-                    req.session.user = acc;
+                    if (err) throw err;
+                    user.password = hash;
+                    user = user.save();
+                    req.session.user = user;
                     res.redirect('/admin')
                 })
             })
-        } catch (err) {
-            console.log(err);
-            res.redirect('/admin/changePass')
+
         }
+    } catch (err) {
+        console.log(err);
     }
 }
